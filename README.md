76040d994338b0599d4e2f68ca4ad11ecd4ba4e0
Teknix <kek@teknix.no>
2016-11-28 14:17:30 +0100

    Fixed cross platform issue
    Tested for Windows and Linux

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ Press **CTRL+L**
 
 You may ignore plugins from the list by adding them to the `plugins_ignore` list in `ListPlugins/listplugins/__init__.py`
 
+**Changelog**
+
+- 28.11.2016:
+ - Fixed cross platform issue (tested for Linux and Windows)
+
 **Screenshot**
 
 ![Screenshot Windows 10](fman-plugin-listplugins-w10.png)
--- listplugins/__init__.py
+++ listplugins/__init__.py
@@ -1,10 +1,17 @@
 from fman import DirectoryPaneCommand, load_json, show_alert
 from os import getenv, path
+import platform
 import glob
 
 class ListPlugins(DirectoryPaneCommand):
     def __call__(self):
-        appdata = getenv('APPDATA')
+        if platform.system() == "Windows":
+            appdata = getenv('APPDATA')
+        elif platform.system() == "Linux":
+            appdata = path.expanduser("~") + "/.config"
+        elif platform.system() == "Darwin":
+            appdata = path.expanduser("~") + "/Library/Application Support/fman"
+
         plugin_dir = glob.glob(appdata + "/fman/Plugins/*")
         plugin_amount = 0
         plugins = []
